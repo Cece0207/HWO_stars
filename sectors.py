99baa98c3531452ce2fd58019860bdb84ad00928
Ciarra <107265822+Cece0207@users.noreply.github.com>
2023-07-19 14:18:25 -0700

    Update sectors.py

--- sectors.py
+++ sectors.py
@@ -30,3 +30,39 @@ def all_per(search):
     std = np.std(periods)
     avg = np.average(periods)
     print('The avgerage is', avg, 'with a std of ', std)
+
+def momentum_dumps
+    TIC = name
+    sectors = []
+    for i in range(len(search)):
+        sector_num = str(search[i].mission)
+        sector_num = sector_num.split(' ')[2]
+        sector_num = int(sector_num[:2])
+        sectors.append(sector_num)
+    
+    
+    for i in sectors:
+
+        tpf = lk.search_targetpixelfile(TIC, mission='TESS',sector = i, author = 'SPOC', cadence = 120 ).download(quality_bitmask = 'none')
+        
+        plt.figure(figsize=(20,10))
+    
+        lc = lk.search_lightcurve(TIC, mission='TESS',sector = i, author = 'SPOC', cadence = 120 ).download(quality_bitmask = 'hard')
+        lc.normalize().scatter()
+ 
+        plt.vlines(tpf.time.value[np.bitwise_and(lc.hdu[1].data['QUALITY'],32) == 32], ymin=0, ymax=2, color = 'mediumseagreen', label = 'Momentum Dumps')
+        
+        f = lc.normalize()
+        f = f[~np.isnan(f['flux'])]
+        min_f = min(f['flux']) - 0.001
+        max_f = max(f['flux']) + 0.001
+        plt.ylim(min_f,max_f)
+  
+        plt.xlabel('Time (TJD)',fontsize = 16)
+        plt.ylabel('Relative Flux', fontsize = 16)
+        plt.title('$'+'Momentum Dump  Check,  Sector  '+ str(i) + '$', fontsize = 20)
+        plt.legend(loc='best', fontsize = 12)
+    
+        plt.show()
+    
+    
