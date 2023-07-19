79250278c15951636ba19f23ec2db4844b3e20ea
Ciarra <107265822+Cece0207@users.noreply.github.com>
2023-07-19 15:34:05 -0700

    Update sectors.py

--- sectors.py
+++ sectors.py
@@ -1,8 +1,10 @@
-def search_result(name):
-    search_result = lk.search_lightcurve(name, author = 'SPOC', cadence = 120)
+def search_result(TIC):
+    search_result = lk.search_lightcurve(TIC, author = 'SPOC', cadence = 120)
     return search_result
 
-def plot_all_sectors(search_result):
+def plot_all_sectors(TIC):
+    search_result = search_result(TIC)
+    
     for i in range(len(search_result)):
         lc = search_result[i].download()
 #         lc.normalize().scatter() 
@@ -10,13 +12,13 @@ def plot_all_sectors(search_result):
         pg = lc.to_periodogram(maximum_period=30)
         pg.plot(view='period', title = '$' + 'Sector: ' + str(lc.sector)+ '$')
 
-
         # Create a model light curve for the highest peak in the periodogram
         lc_model = pg.model(time=lc.time, frequency=pg.frequency_at_max_power)
         ax = lc.normalize().scatter()
         lc_model.scatter(ax=ax, lw=3, ls='--', c='red', label = '$' + 'Sector: ' + str(lc.sector)+ '$')
 
-def all_per(search):
+def all_per(TIC):
+    search = search_result(TIC)
     periods = []
     avg = 0
     std = 0
@@ -31,8 +33,10 @@ def all_per(search):
     avg = np.average(periods)
     print('The avgerage is', avg, 'with a std of ', std)
 
-def momentum_dumps
-    TIC = name
+def momentum_dumps(TIC)
+
+    search = search_result(TIC)
+    
     sectors = []
     for i in range(len(search)):
         sector_num = str(search[i].mission)
